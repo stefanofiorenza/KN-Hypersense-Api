d4be08993a8e9c914cd6691ff6a2914c0bbd7a49
Dmitri Marinen <dmitri.marinen@gmail.com>
2021-05-10 19:12:36 +0300

    Initial version of coreplatform generated by JHipster-7.0.1

---
+++ src/main/java/com/knits/coreplatform/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,10 @@
+package com.knits.coreplatform.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+}
