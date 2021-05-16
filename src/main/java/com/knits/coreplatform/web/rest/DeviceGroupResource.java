a4eee63f64b274014c884c00a1ec582bb954a252
ExiledRain <gett_lostt@bk.ru>
2021-05-16 15:26:16 +0300

    KHS-59 CompanyAdmin can create Device Group.

--- src/main/java/com/knits/coreplatform/web/rest/DeviceGroupResource.java
+++ src/main/java/com/knits/coreplatform/web/rest/DeviceGroupResource.java
@@ -1,6 +1,7 @@
 package com.knits.coreplatform.web.rest;
 
 import com.knits.coreplatform.repository.DeviceGroupRepository;
+import com.knits.coreplatform.security.AuthoritiesConstants;
 import com.knits.coreplatform.service.DeviceGroupService;
 import com.knits.coreplatform.service.dto.DeviceGroupDTO;
 import com.knits.coreplatform.web.rest.errors.BadRequestAlertException;
@@ -13,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import tech.jhipster.web.util.HeaderUtil;
 import tech.jhipster.web.util.ResponseUtil;
@@ -48,6 +50,7 @@ public class DeviceGroupResource {
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
     @PostMapping("/device-groups")
+    @PreAuthorize("hasAuthority(\"" + AuthoritiesConstants.COMPANY_ADMIN + "\")")
     public ResponseEntity<DeviceGroupDTO> createDeviceGroup(@RequestBody DeviceGroupDTO deviceGroupDTO) throws URISyntaxException {
         log.debug("REST request to save DeviceGroup : {}", deviceGroupDTO);
         if (deviceGroupDTO.getId() != null) {
